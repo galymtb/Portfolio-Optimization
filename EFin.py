736671e9548c2be639a73970be4053d1bfcddb21
Galym <torebaevgalym@gmail.com>
2023-02-08 17:51:27 +0800

    first

---
+++ EFin.py
@@ -0,0 +1,151 @@
+import numpy as np
+import pandas as pd
+import scipy.optimize as sc
+import datetime as dt
+from pandas_datareader import data as pdr
+import plotly.graph_objects as go
+import yfinance as yf
+
+yf.pdr_override()
+
+def getData(stocks, start, end):
+    stockData = pdr.get_data_yahoo(stocks, start=start, end=end)
+    stockData = stockData['Close']
+    
+    returns = stockData.pct_change()
+    meanReturn = returns.mean()
+    covMatrix = returns.cov()
+    return meanReturn, covMatrix
+
+def portfolioPerformance(weights, meanReturns, covMatrix):
+    returns = np.sum(meanReturns*weights)*252
+    std = np.sqrt(
+            np.dot(weights.T,np.dot(covMatrix, weights))
+           )*np.sqrt(252)
+    return returns, std
+
+def negativeSR(weights, meanReturns, covMatrix, riskFreeRate = 0):
+    pReturns, pStd = portfolioPerformance(weights, meanReturns, covMatrix)
+    return - (pReturns - riskFreeRate)/pStd
+
+def maxSR(meanReturns, covMatrix, riskFreeRate = 0, constraintSet=(0,1)):
+    numAssets = len(meanReturns)
+    args = (meanReturns, covMatrix, riskFreeRate)
+    constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+    bound = constraintSet
+    bounds = tuple(bound for asset in range(numAssets))
+    result = sc.minimize(negativeSR, numAssets*[1./numAssets], args=args,
+                        method='SLSQP', bounds=bounds, constraints=constraints)
+    return result
+
+def portfolioVariance(weights, meanReturns, covMatrix):
+    return portfolioPerformance(weights, meanReturns, covMatrix)[1]
+
+def minimizeVariance(meanReturns, covMatrix, constraintSet=(0,1)):
+    numAssets = len(meanReturns)
+    args = (meanReturns, covMatrix)
+    constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+    bound = constraintSet
+    bounds = tuple(bound for asset in range(numAssets))
+    result = sc.minimize(portfolioVariance, numAssets*[1./numAssets], args=args,
+                        method='SLSQP', bounds=bounds, constraints=constraints)
+    return result
+
+def portfolioReturn(weights, meanReturns, covMatrix):
+    return portfolioPerformance(weights, meanReturns, covMatrix)[0]
+
+
+def efficientOpt(meanReturns, covMatrix, returnTarget, constraintSet=(0,1)):
+    numAssets = len(meanReturns)
+    args = (meanReturns, covMatrix)
+
+    constraints = ({'type':'eq', 'fun': lambda x: portfolioReturn(x, meanReturns, covMatrix) - returnTarget},
+                    {'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+    bound = constraintSet
+    bounds = tuple(bound for asset in range(numAssets))
+    effOpt = sc.minimize(portfolioVariance, numAssets*[1./numAssets], args=args, method = 'SLSQP', bounds=bounds, constraints=constraints)
+    return effOpt
+
+def calculatedResults(meanReturns, covMatrix, riskFreeRate=0, constraintSet=(0,1)):
+    # Max Sharpe Ratio Portfolio
+    maxSR_Portfolio = maxSR(meanReturns, covMatrix)
+    maxSR_returns, maxSR_std = portfolioPerformance(maxSR_Portfolio['x'], meanReturns, covMatrix)
+    maxSR_allocation = pd.DataFrame(maxSR_Portfolio['x'], index=meanReturns.index, columns=['allocation'])
+    maxSR_allocation.allocation = [round(i*100,0) for i in maxSR_allocation.allocation]
+    
+    # Min Volatility Portfolio
+    minVol_Portfolio = minimizeVariance(meanReturns, covMatrix)
+    minVol_returns, minVol_std = portfolioPerformance(minVol_Portfolio['x'], meanReturns, covMatrix)
+    minVol_allocation = pd.DataFrame(minVol_Portfolio['x'], index=meanReturns.index, columns=['allocation'])
+    minVol_allocation.allocation = [round(i*100,0) for i in minVol_allocation.allocation]
+
+    # Efficient Frontier
+    efficientList = []
+    targetReturns = np.linspace(minVol_returns, maxSR_returns, 20)
+    for target in targetReturns:
+        efficientList.append(efficientOpt(meanReturns, covMatrix, target)['fun'])
+
+    maxSR_returns, maxSR_std = round(maxSR_returns*100,2), round(maxSR_std*100,2)
+    minVol_returns, minVol_std = round(minVol_returns*100,2), round(minVol_std*100,2)
+
+    return maxSR_returns, maxSR_std, maxSR_allocation, minVol_returns, minVol_std, minVol_allocation, efficientList, targetReturns
+
+def EF_graph(meanReturns, covMatrix, riskFreeRate=0, constraintSet=(0,1)):
+    maxSR_returns, maxSR_std, maxSR_allocation, minVol_returns, minVol_std, minVol_allocation, efficientList, targetReturns = calculatedResults(meanReturns, covMatrix, riskFreeRate, constraintSet)
+
+    #Max SR
+    MaxSharpeRatio = go.Scatter(
+        name='Maximium Sharpe Ratio',
+        mode='markers',
+        x=[maxSR_std],
+        y=[maxSR_returns],
+        marker=dict(color='red',size=14,line=dict(width=3, color='black'))
+    )
+
+    #Min Vol
+    MinVol = go.Scatter(
+        name='Mininium Volatility',
+        mode='markers',
+        x=[minVol_std],
+        y=[minVol_returns],
+        marker=dict(color='green',size=14,line=dict(width=3, color='black'))
+    )
+
+    #Efficient Frontier
+    EF_curve = go.Scatter(
+        name='Efficient Frontier',
+        mode='lines',
+        x=[round(ef_std*100, 2) for ef_std in efficientList],
+        y=[round(target*100, 2) for target in targetReturns],
+        line=dict(color='black', width=4, dash='dashdot')
+    )
+
+    data = [MaxSharpeRatio, MinVol, EF_curve]
+
+    layout = go.Layout(
+        title = 'Portfolio Optimisation with the Efficient Frontier',
+        yaxis = dict(title='Annualised Return (%)'),
+        xaxis = dict(title='Annualised Volatility (%)'),
+        showlegend = True,
+        legend = dict(
+            x = 0.75, y = 0, traceorder='normal',
+            bgcolor='#E2E2E2',
+            bordercolor='black',
+            borderwidth=2),
+        width=800,
+        height=600)
+    
+    fig = go.Figure(data=data, layout=layout)
+    return fig.show()
+    
+def main():
+    stockList = ['AAPL', 'TSLA', 'GOOG']
+    stocks = [stock for stock in stockList]
+
+    endDate = dt.datetime.now()
+    startDate = endDate - dt.timedelta(days=365)
+    
+    meanReturns, covMatrix = getData(stocks, start=startDate, end=endDate)
+    EF_graph(meanReturns, covMatrix)
+
+main()
